3d0feb7d9c00c413111d8044d3e0004e19d7e18d
HuviXYZ <h.nygjerde2@gmail.com>
2021-10-08 14:31:48 +0200

    new

---
+++ js/menutoggle.js
@@ -0,0 +1,6 @@
+
+function toggleMenu() { /* lager funkjon TOGGLE */
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+let x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu; 
